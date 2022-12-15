f16a31d3e708f482a9490f0061c9f1fcba941bc7
Yan, Xiping <xiping.yan@intel.com>
2022-12-15 10:22:46 +0800

    add submodule onednn

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "my_test.hpp"
 
+#include <iostream>
+
 int main(int argc, char **argv)
 {
     // getting_started_test_relu(argc, argv);
     getting_started_test_conv(argc, argv);
-    return 0;
+    return EXIT_SUCCESS;
 }
